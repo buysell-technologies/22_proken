00a15503536ef66514383a1b9f4536c5ed348f4e
keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
2022-05-16 18:55:42 +0900

    routeを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "respond", to: 'thank#respond'
+  namespace :slack do
+    post 'events', to: 'tank#respond'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
