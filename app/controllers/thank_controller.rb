bc293500efb731e99b637d709a56f8ac87b75bec
keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
2022-05-16 18:43:49 +0900

    ホームタブ作成

--- app/controllers/thank_controller.rb
+++ app/controllers/thank_controller.rb
@@ -6,4 +6,34 @@ class ThankController < ApplicationController
   def send(message)
     Slack::Notifier.new(WEBHOOK_URL, channel: CHANNEL, username: USER_NAME).ping('message')
   end
+
+  def respond
+    if params[:event][:type] == 'app_home_opened'
+      views_publish
+    elsif params[:event][:type] == 'message' && params[:event][:text].present?
+      p "ここは使い方でよさそう"
+    end
+  end
+
+  def views_publish
+    team = Team.find_by(workspace_id: params[:team_id])
+    user = User.find_by(uid: params[:event][:user])
+    return if team.nil? || user.nil? || team.workspace_id != user.team.workspace_id
+
+    access_token = set_access_token(user.authentication.access_token)
+    publish_to_home_tab(team, user, access_token)
+  end
+
+  def publish_to_home_tab(team, user, access_token)
+    encoded_msg = encoded_home_tab_block_msg(team)
+    access_token.post("api/views.publish?user_id=#{user.uid}&view=#{encoded_msg}&pretty=1").parsed
+  end
+
+  def encoded_home_tab_block_msg(team)
+    channel_id = team.share_channel_id
+    channel_name = team.share_channel_name
+      msg = "{ここにjson貼ってくれーー}"
+    encoded_msg = ERB::Util.url_encode(msg)
+    encoded_msg
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
-  get "hello", to: 'application#hello'
-  get "send", to: 'thank#send'
-
-  resources :sessions, only: [:create, :destroy]
+  get "respond", to: 'thank#respond'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
