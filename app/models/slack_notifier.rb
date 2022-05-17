7504d24f391655273c817cbd82d8fcad8761c2eb
Tamarihiroto <h.tamari@buysell-technologies.com>
2022-05-17 14:43:50 +0900

    feat: add message method

--- app/models/slack_notifier.rb
+++ app/models/slack_notifier.rb
@@ -11,10 +11,116 @@ class SlackNotifier
   end
 
   def send(sender, recipient, value, message)
-    payload = {
-      fallback: "send thanks",
-      text: message
-    }
-    client.post text: "#{sender}さんが#{recipient}さんに「#{value}」しました", attachments: [payload]
+    client.post blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "plain_text",
+          "text": "#{sender}さんが#{recipient}さんに「#{value}」しました",
+          "emoji": true
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "> #{message}"
+        }
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "#{recipient}さんに「#{value}」を送りますか",
+              "emoji": true
+            },
+            "value": "click_me_123",
+            "action_id": "actionId-0"
+          }
+        ]
+      }
+    ]
+  end
+
+  def hoge()
+    client.post blocks: [
+      {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": "感謝を送ろう",
+          "emoji": true
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "multi_users_select",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "選択",
+            "emoji": true
+          },
+          "action_id": "multi_users_select-action"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "メンバーの選択",
+          "emoji": true
+        }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "multi_users_select",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "選択",
+            "emoji": true
+          },
+          "action_id": "multi_users_select-action"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "バリューの選択",
+          "emoji": true
+        }
+      },
+      {
+        "type": "input",
+        "element": {
+          "type": "plain_text_input",
+          "multiline": true,
+          "action_id": "plain_text_input-action"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "感謝する内容",
+          "emoji": true
+        }
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "emoji": true,
+              "text": "送信"
+            },
+            "style": "primary",
+            "value": "click_me_123",
+            "action_id": "actionId-0"
+          }
+        ]
+      }
+    ]
   end
 end
