13395b872b3a4773be750af89045a3f71be91f28
keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
2022-05-17 15:20:43 +0900

    nginx

---
+++ config/puma/production.rb
@@ -0,0 +1,11 @@
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
