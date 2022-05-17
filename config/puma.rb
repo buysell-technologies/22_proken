747bf7ae003b1fe75633ad5a3557b37289e3b505
keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
2022-05-17 19:49:03 +0900

    aaa

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -13,4 +13,3 @@ bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
