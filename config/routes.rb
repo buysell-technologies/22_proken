f4938970f05eb74e50a6926b335c98cf32f4c607
Tamarihiroto <h.tamari@buysell-technologies.com>
2022-05-17 18:30:34 +0900

    'の削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :slack do
-    post "/receive", to: "slack#create"'
+    post "/receive", to: "slack#create"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
