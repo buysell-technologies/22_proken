2585aad98cbcf6f639b9d00513f6d3d78c615242
Tamarihiroto <h.tamari@buysell-technologies.com>
2022-05-17 18:29:20 +0900

    エンドポイントの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :slack do
-    post 'events', to: 'thank#respond'
+    post "/receive", to: "slack#create"'
   end
-  post "/thank"=>"thank#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
