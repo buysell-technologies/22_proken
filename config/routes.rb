6cc3b910a0415ef3c67fdc5adae4bfa3c841d072
Tamarihiroto <h.tamari@buysell-technologies.com>
2022-05-17 18:31:31 +0900

    ルーチングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :slack do
-    post "/receive", to: "slack#create"
-  end
+  post "/receive"=>"slack#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
