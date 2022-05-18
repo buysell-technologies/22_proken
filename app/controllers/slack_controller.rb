74fbdfa5495f8471e16009fa8f751f99d8d6c116
keito-sakakibara <62434610+keito-sakakibara@users.noreply.github.com>
2022-05-18 15:15:53 +0900

    検証

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -71,12 +71,9 @@ class SlackController < ApplicationController
   def views_publish()
     user_uid = params[:event][:user]
     client = Slack::Web::Client.new
-    pp "user_id"
-    pp user_uid
-    pp "client"
-    pp client.users_info(user: user_uid)
-    pp "333333333"
-    client.views_publish(
+    pp client
+    pp "22222222222"
+    pp client.views_publish(
       token=ENV['BOT_USER_ACCESS_TOKEN'],
       user_id=user_uid,
       view='{"type":"home","blocks":[{"type":"header","text":{"type":"plain_text","text":"感謝を送ろう","emoji":true}},{"type":"divider"},{"type":"input","element":{"type":"users_select","placeholder":{"type":"plain_text","text":"選択","emoji":true},"action_id":"users_select-action"},"label":{"type":"plain_text","text":"メンバーの選択","emoji":true}},{"type":"input","element":{"type":"static_select","placeholder":{"type":"plain_text","text":"選択","emoji":true},"options":[{"text":{"type":"plain_text","text":"さすが","emoji":true},"value":"value-0"},{"text":{"type":"plain_text","text":"すぐ","emoji":true},"value":"value-1"},{"text":{"type":"plain_text","text":"おそれず","emoji":true},"value":"value-2"},{"text":{"type":"plain_text","text":"みずから","emoji":true},"value":"value-3"}],"action_id":"multi_static_select-action"},"label":{"type":"plain_text","text":"バリューの選択","emoji":true}},{"type":"input","element":{"type":"plain_text_input","multiline":true,"action_id":"plain_text_input-action"},"label":{"type":"plain_text","text":"感謝する内容","emoji":true}},{"type":"actions","elements":[{"type":"button","text":{"type":"plain_text","emoji":true,"text":"送信"},"style":"primary","value":"click_me_123","action_id":"actionId-1"}]}]}'
